5aeb2215ab14415485b7075f75cf559418b03755
ntnguyen041 <nt.nguyen041@gmail.com>
2023-06-08 15:59:25 +0700

    client

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Layout from './Components/Layout/layout';
 import {io} from "socket.io-client";
 
 function App() {
+  
   const [username,setUsername]=useState("");
   const[user,setUser]=useState("");
-
   useEffect(()=>{
     const socket = io("https://server-smart-home.onrender.com");
     console.log(socket);
