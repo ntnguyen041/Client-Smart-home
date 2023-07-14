f44318a4910c50cce695d5f0b8194a081c545ea8
ntnguyen041 <nt.nguyen041@gmail.com>
2023-07-14 21:04:54 +0700

    nguyen

--- src/socket/socket.jsx
+++ src/socket/socket.jsx
@@ -2,4 +2,4 @@ import { io } from "socket.io-client";
 //const socket = io.connect(['http://localhost:3001','https://server-smart-home.onrender.com'], { reconnect: true });
 const socket = io.connect('https://server-smart-home.onrender.com', { reconnect: true });
 
-export default socket
+export default socket 
